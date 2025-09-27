72eea6f57e6fe37ce4e5b305c2882a1f6562e2bb
engahmad9797 <ahmed.rande97@gmail.com>
2025-09-27 10:08:38 +0300

    إنشاء ملف js/config.js لإعدادات Firebase

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// إعدادات عامة للمشروع (مثال: إعدادات Firebase)
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+// يمكنك تصدير الإعدادات إذا كنت تستخدم ES6 Modules
+// export default firebaseConfig;
