0b182a31da3bb04edb3bcebc7c73987adcbd6c56
engahmad9797 <ahmed.rande97@gmail.com>
2025-09-27 10:28:43 +0300

    Update tools.js

--- js/tools.js
+++ js/tools.js
@@ -10,3 +10,27 @@ function generateQRCode(data) {
 
 // يمكنك تصدير الدوال إذا كنت تستخدم ES6 Modules
 // export { generateShortLink, generateQRCode };
+// tools.js
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+
+function generateId(prefix = '') {
+  return prefix + crypto.randomBytes(8).toString('hex');
+}
+
+function log(msg, type='info') {
+  const time = new Date().toISOString();
+  if (type === 'error') console.error(`[ERROR] ${time} - ${msg}`);
+  else if (type === 'warn') console.warn(`[WARN] ${time} - ${msg}`);
+  else console.log(`[INFO] ${time} - ${msg}`);
+}
+
+async function sendEmail(to, subject, text) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS }
+  });
+  return transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, text });
+}
+
+module.exports = { generateId, log, sendEmail };
